ddc81260ff1f1d9f57cc1ceb9c1742ab07f8c7af
liqlvnvn <liqlvnvn@gmail.com>
2021-05-04 11:40:52 +0300

    Add logging message about a start of the server.

--- pkg/server/server.go
+++ pkg/server/server.go
@@ -9,6 +9,7 @@ import (
 )
 
 func ServeHTTP(cfg config.Config, content string) {
+	fmt.Printf("Starting the server. You can reach the output on %v%v%v\n", cfg.HostName, cfg.Port, cfg.MetricsPath)
 	http.HandleFunc(cfg.MetricsPath, func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprint(w, content)
 	})
